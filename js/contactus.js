59e295d21791519d2750b7cd0e176eb0a18e1ff8
nawteeh-8 <neo.ignite45@gmail.com>
2025-07-31 18:29:12 -0400

    Use global sanitize function

--- js/contactus.js
+++ js/contactus.js
@@ -1,20 +1,8 @@
-  function sanitizeForm(form) {
-    const fields = form.querySelectorAll('input, textarea');
-    for (const field of fields) {
-      const val = field.value.trim();
-      if (/[<>]/.test(val) || /script/i.test(val)) {
-        return false;
-      }
-      field.value = val;
-    }
-    return true;
-  }
-
   document.addEventListener('DOMContentLoaded', () => {
     const contactForm = document.getElementById('contactForm');
     contactForm.addEventListener('submit', e => {
       e.preventDefault();
-      if (!sanitizeForm(contactForm)) {
+      if (!window.sanitizeForm(contactForm)) {
         alert('Suspicious content detected. Submission rejected.');
         return;
       }
